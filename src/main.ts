c2d557c9039cb6188ded27eabef83c807bb1a9bc
Stephen Zhou <hi@hyoban.cc>
2024-03-09 14:26:36 +0800

    chore: install unocss and set path

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,7 @@
-import { createApp } from 'vue'
+import 'virtual:uno.css'
 import './style.css'
+
+import { createApp } from 'vue'
 import App from './App.vue'
 
 createApp(App).mount('#app')
---
+++ uno.config.ts
@@ -0,0 +1,21 @@
+import { defineConfig, presetUno } from 'unocss'
+import presetAnimations from 'unocss-preset-animations'
+import { presetShadcn } from 'unocss-preset-shadcn'
+
+export default defineConfig({
+  presets: [
+    presetUno(),
+    presetAnimations(),
+    presetShadcn(),
+  ],
+  content: {
+    pipeline: {
+      include: [
+        // the default
+        /\.(vue|svelte|[jt]sx|mdx?|astro|elm|php|phtml|html)($|\?)/,
+        // include js/ts files
+        'src/**/*.{js,ts}',
+      ],
+    },
+  },
+});
